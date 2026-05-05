5f88f62eaf519d44263227d027bf717052601791
AndresLeitonDev <andresh.leiton@gmail.com>
2026-05-05 01:56:36 -0500

    fix puerto render

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
